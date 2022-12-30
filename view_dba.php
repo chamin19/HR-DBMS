873cb775559649a61fdef1d3bbe1d2393aeeb9b8
camillia <hanaan.amin@yahoo.com>
2022-12-30 18:07:37 -0500

    added table view

--- view_dba.php
+++ view_dba.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html> 
+    <?php include ('dbconnect.php'); ?>
     <head>
         <meta charset="UTF-8">
         <meta name = "description" content = "main page">
@@ -17,7 +18,7 @@
     <body>
         <header>
             <div class="logo">
-                <a class="button" href="main.html" style="text-decoration: none;"><h2>HR Payroll DBMS</h2></a>
+                <a class="button" href="https://www.cs.ryerson.ca/~chamin/hr_payroll/main.html" style="text-decoration: none;"><h2>HR Payroll DBMS</h2></a>
             </div>
             <div class="profile">
                 <h4>Database Administrator</h4>
@@ -25,19 +26,253 @@
         </header>
         <main>
             <br>
+            <button data-toggle="collapse" data-target="#demo">Collapsible</button>
+            <div class="collapse">
+                tables
+            </div>
             <div class="container">
                 <div class="row">
                     <div class="col-sm-8" id="tables">
-                       tables
+                        <?php
+                            $sql = "SELECT * FROM emp
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if (mysqli_num_rows($result) > 0) {
+                                echo "<table>";
+                                echo "<tr><th>Emp ID</th>";
+                                echo "<th>First Name</th>";
+                                echo "<th>Last Name</th>";
+                                echo "<th>Email</th>";
+                                echo "<th>Phone</th>";
+                                echo "<th>Street No.</th>";
+                                echo "<th>Street Name</th>";
+                                echo "<th>City</th>";
+                                echo "<th>Province</th>";
+                                echo "<th>Postal Code</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["emp_id"] . "</td>";
+                                    echo "<td>" . $row["emp_first_name"] . "</td>";
+                                    echo "<td>" . $row["emp_last_name"] . "</td>";
+                                    echo "<td>" . $row["emp_email"] . "</td>";
+                                    echo "<td>" . $row["emp_phone"] . "</td>";
+                                    echo "<td>" . $row["emp_address_street_number"] . "</td>";
+                                    echo "<td>" . $row["emp_address_street_name"] . "</td>";
+                                    echo "<td>" . $row["emp_address_city"] . "</td>";
+                                    echo "<td>" . $row["emp_address_province"] . "</td>";
+                                    echo "<td>" . $row["emp_address_postal_code"] ."</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM emp_dept
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Employee ID</th>";
+                                echo "<th>Department ID</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["emp_id"] . "</td>";
+                                    echo "<td>" . $row["dept_id"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM dept
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Department ID</th>";
+                                echo "<th>Department Name</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["dept_id"] . "</td>";
+                                    echo "<td>" . $row["dept_name"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM emp_bank_account
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Employee ID</th>";
+                                echo "<th>Account ID</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["emp_id"] . "</td>";
+                                    echo "<td>" . $row["account_id"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM bank_account
+                            ORDER BY account_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Account ID</th>";
+                                echo "<tr><th>Transit No.</th>";
+                                echo "<tr><th>Institution No.</th>";
+                                echo "<th>Account No.</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["account_id"] . "</td>";
+                                    echo "<tr> <td>" . $row["transit_number"] . "</td>";
+                                    echo "<tr> <td>" . $row["institution_number"] . "</td>";
+                                    echo "<tr> <td>" . $row["account_number"] . "</td>";
+                                    echo "<td>" . $row["account_balance"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM account_payment
+                            ORDER BY account_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Account ID</th>";
+                                echo "<th>Payment ID</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["account_id"] . "</td>";
+                                    echo "<td>" . $row["payment_id"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM payment
+                            ORDER BY account_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Payment ID</th>";
+                                echo "<th>Paystub Amount ($CAD)</th>";
+                                echo "<th>Payment Date</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["payment_id"] . "</td>";
+                                    echo "<td>" . $row["pay_stub_amount"] . "</td>";
+                                    echo "<td>" . $row["payment_date"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM emp_position
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Employee ID</th>";
+                                echo "<th>Position ID</th>";
+                                echo "<th>Start Date</th>";
+                                echo "<th>End Date</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["emp_id"] . "</td>";
+                                    echo "<td>" . $row["position_id"] . "</td>";
+                                    echo "<td>" . $row["position_start_date"] . "</td>";
+                                    if ($row["position_end_date"] == "NULL") {
+                                        echo "<td>None";
+                                    } else {
+                                        echo "<td>" . $row["position_end_date"];
+                                    }
+                                    echo "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM position_table
+                            ORDER BY position_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Position ID</th>";
+                                echo "<th>Position Title</th>";
+                                echo "<th>Start Date</th>";
+                                echo "<th>End Date</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["position_id"] . "</td>";
+                                    echo "<td>" . $row["position_title"] . "</td>";
+                                    echo "<td>" . $row["positiontype_a"] . "</td>";
+                                    echo "<td>" . $row["positiontype_b"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM emp_work_period
+                            ORDER BY emp_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Employee ID</th>";
+                                echo "<th>Work Period Title</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["emp_id"] . "</td>";
+                                    echo "<td>" . $row["work_period_id"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+
+                            $sql = "SELECT * FROM work_period
+                            ORDER BY work_period_id ASC;";
+                            $result = mysqli_query($connect, $sql);
+                            // mysqli_num_rows($result) > 0
+                            if ($result) {
+                                echo "<table>";
+                                echo "<tr><th>Work Period ID</th>";
+                                echo "<th>Start Time</th>";
+                                echo "<th>End Time</th></tr>";
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr> <td>" . $row["work_period_id"] . "</td>";
+                                    echo "<td>" . $row["start_time"] . "</td>";
+                                    echo "<td>" . $row["end_time"] . "</td><tr>";
+                                }
+                                echo "</table><br>";
+                            } else {
+                            echo "No results.";
+                            }
+                        ?>
                     </div>
                     <div class="col-md-4">
                        <div class="row">
-                          <div class="col-sm-12" id="output">
-                            output
-                          </div>
-                          <div class="col-sm-12" id="editor">
-                            editor
-                          </div>
+                        <div class="col-sm-12" id="editor">
+                                    <form>
+                                        <textarea rows = "7" cols = "60" name = "query" id="query" spellcheck="false" autocorrect="off" autocapitalize="off">SELECT department_id, COUNT(*)&#10;FROM employee_position, employee_department, position_table&#10;WHERE employee_position.employee_id = employee_department.employee_id&#10;AND position_table.position_id = employee_position.position_id&#10;AND position_table.position_title != 'Intern'&#10;GROUP BY department_id;
+                                        </textarea><br>
+                                        <input type="submit" value="Run query">
+                                    </form>
+                                </div>     
+                            <div class="col-sm-12" id="output">
+                                output
+                            </div>
+                            
                        </div>
                     </div>
                 </div>
@@ -49,22 +284,52 @@
             cursor: pointer;
         }
         .container {
-            width: 95%;
+            width: 100%;
         }
         #tables {
-            background-color: blanchedalmond;
+            background-color: #d3eaf2;
             height: 500px;
             overflow-y: scroll;
+            padding: 10px;
         }
         #output {
-            background-color: blue;
+            background-color: #f3f5f6;
             height: 300px;
             overflow-y: scroll;
+            padding: 10px;
+        }
+        /* form {
+            width: 100%;
+            padding: 0px;
+            margin: 0px;
+        } */
+        textarea {
+            background-color: #2A4895;
+            color: white;
+            width: 100%;
+            height: 100%;
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 14px;
         }
         #editor {
-            background-color: blueviolet;
             height: 200px;
-            overflow-y: scroll;
+            width: 100%;
+        }
+        table {
+            font-family: 'Courier New', Courier, monospace;
+            border: 2px solid black;
+            font-size: 14px;
+            border-collapse: separate;
+            border-spacing: 3px;
+            background-color: white;
+        }
+        th {
+            background: #2A4895;
+            color: white;
+            font-size: 14px;
+        }
+        tr{
+            background: white;
         }
     </style>
 </html>
