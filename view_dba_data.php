7d315ba5d6fc49b2478760a896bd5e6ef0d121ae
camillia <hanaan.amin@yahoo.com>
2023-01-04 13:11:23 -0500

    fixed typo

--- view_dba_data.php
+++ view_dba_data.php
@@ -371,12 +371,22 @@
                                     </div>
                                 </div>
                                 <?php
-                                if (isset($_POST['emp_dept_apply'])){
-                                    echo '<script>alert("worked");</script>';
-                                    include ('view_dba_edit.php'); 
+                            }
+                            if (isset($_POST['emp_dept_apply'])){
+                                $emp_id = $_POST['emp_id_edit'];
+                                $new_dept_id = $_POST['dept_id_edit'];
+                                $edit_emp_dept = 'UPDATE emp_dept SET
+                                                dept_id = ' . $new_dept_id .'
+                                                WHERE emp_id = ' . $emp_id;
+                                $result = mysqli_query($connect, $edit_emp_dept);
+                                if ($result) {
+                                    echo "<meta http-equiv='refresh' content='0'>";
+                                } else {
+                                    ?>
+                                        <script>alert("<?php echo 'Error updating record: ' . mysqli_error($connect)?> ")</script>
+                                    <?php 
                                 }
                             }
-
 
                             $sql = "SELECT * FROM dept
                             ORDER BY dept_id ASC;";
@@ -448,9 +458,20 @@
                                     </div>
                                 </div>
                                 <?php
-                                if (isset($_POST['dept_apply'])){
-                                    echo '<script>alert("worked");</script>';
-                                    include ('view_dba_edit.php'); 
+                            }
+
+                            if (isset($_POST['dept_apply'])){
+                                $dept_id = $_POST['dept_id_edit'];
+                                $new_dept_name = $_POST['dept_name_edit'];
+                                $edit_dept = 'UPDATE dept SET dept_name = "' . $new_dept_name . '"
+                                    WHERE dept_id = ' . $dept_id;
+                                $result = mysqli_query($connect, $edit_dept);
+                                if ($result) {
+                                    echo "<meta http-equiv='refresh' content='0'>";
+                                } else {
+                                    ?>
+                                        <script>alert("<?php echo 'Error updating record: ' . mysqli_error($connect)?> ")</script>
+                                    <?php 
                                 }
                             }
 
@@ -569,12 +590,26 @@
                                     </div>
                                 </div>
                                 <?php
-                                if (isset($_POST['bank_apply'])){
-                                    echo '<script>alert("worked");</script>';
-                                    include ('view_dba_edit.php'); 
+                            }
+                            if (isset($_POST['bank_apply'])){
+                                $acc_id = $_POST['account_id_edit'];
+                                $new_tran_number = $_POST['tran_number_edit'];
+                                $new_inst_number = $_POST['inst_number_edit'];
+                                $new_acc_number = $_POST['acc_number_edit'];
+                                $edit_bank = 'UPDATE bank_account SET 
+                                    transit_number = ' . $new_tran_number .', 
+                                    institution_number = ' . $new_inst_number .', 
+                                    account_number = ' . $new_acc_number .'
+                                    WHERE account_id = ' . $acc_id;
+                                $result = mysqli_query($connect, $edit_bank);
+                                if ($result) {
+                                    echo "<meta http-equiv='refresh' content='0'>";
+                                } else {
+                                    ?>
+                                        <script>alert("<?php echo 'Error updating record: ' . mysqli_error($connect)?> ")</script>
+                                    <?php 
                                 }
                             }
-
 
                             $sql = "SELECT * FROM account_payment
                             ORDER BY account_id ASC;";
@@ -728,9 +763,24 @@
                                     </div>
                                 </div>
                                 <?php
-                                if (isset($_POST['emp_position_apply'])){
-                                    echo '<script>alert("worked");</script>';
-                                    include ('view_dba_edit.php'); 
+                            }
+                            if (isset($_POST['emp_position_apply'])){
+                                $emp_id = $_POST['emp_id_edit'];
+                                $emp_position_id = $_POST['position_id_edit'];
+                                $new_start_date = $_POST['start_date_edit'];
+                                $new_end_date = $_POST['end_date_edit'];
+                                $edit_emp_position = 'UPDATE emp_position SET 
+                                    position_id = ' . $emp_position_id .', 
+                                    position_start_date = "' . $new_start_date .'",
+                                    position_end_date = "' . $new_end_date .'"
+                                    WHERE emp_id = ' . $emp_id;
+                                $result = mysqli_query($connect, $edit_emp_position);
+                                if ($result) {
+                                    echo "<meta http-equiv='refresh' content='0'>";
+                                } else {
+                                    ?>
+                                        <script>alert("<?php echo 'Error updating record: ' . mysqli_error($connect)?> ")</script>
+                                    <?php 
                                 }
                             }
 
@@ -815,11 +865,28 @@
                                     </div>
                                 </div>
                                 <?php
-                                if (isset($_POST['position_table_apply'])){
-                                    echo '<script>alert("worked");</script>';
-                                    include ('view_dba_edit.php'); 
+                            }
+                            if (isset($_POST['position_table_apply'])){
+                                $position_id = $_POST['position_id_edit'];
+                                $new_position_title = $_POST['position_title_edit'];
+                                $new_positiontype_a = $_POST['positiontype_a_edit'];
+                                $new_positiontype_b = $_POST['positiontype_b_edit'];
+                                $edit_position_table = 'UPDATE position_table SET 
+                                    position_title = "' . $new_position_title .'",
+                                    positiontype_a = "' . $new_positiontype_a .'",
+                                    positiontype_b = "' . $new_positiontype_b .'"
+                                    WHERE position_id = $position_id';
+                                $result = mysqli_query($connect, $edit_position_table);
+                                if ($result) {
+                                    echo "<meta http-equiv='refresh' content='0'>";
+                                } else {
+                                    ?>
+                                        <script>alert("<?php echo 'Error updating record: ' . mysqli_error($connect)?> ")</script>
+                                    <?php 
                                 }
                             }
+
+                            
 
                             $sql = "SELECT * FROM emp_work_period
                             ORDER BY emp_id ASC;";
